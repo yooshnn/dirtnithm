92401690324e6fe8dcf34bc8245d7c44243b2900
cupya <me@cupya.me>
2026-07-11 14:53:46 +0900

    feat: configure DI container and Serilog logging on startup

--- Dirtnithm.App/App.xaml.cs
+++ Dirtnithm.App/App.xaml.cs
@@ -1,14 +1,39 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.Hosting;
 using System.Windows;
+using Serilog;
+using Microsoft.Extensions.DependencyInjection;
+using Dirtnithm.App.Services;
+using Dirtnithm.App.Infrastructure;
 
-namespace Dirtnithm.App
+namespace Dirtnithm.App;
+
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    private IHost _host = null!;
+
+    protected override void OnStartup(StartupEventArgs e)
     {
+        base.OnStartup(e);
+
+        LoggingConfig.Configure();
+
+        _host = Host.CreateDefaultBuilder()
+            .UseSerilog()
+            .ConfigureServices(services =>
+            {
+                services.AddTransient<MainWindow>();
+                services.AddSingleton<SettingsService>();
+            })
+            .Build();
+
+        var window = _host.Services.GetRequiredService<MainWindow>();
+        window.Show();
     }
 
+    protected override void OnExit(ExitEventArgs e)
+    {
+        _host.Dispose();
+        Log.CloseAndFlush();
+        base.OnExit(e);
+    }
 }
---
+++ Dirtnithm.App/Infrastructure/LoggingConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Serilog;
+
+namespace Dirtnithm.App.Infrastructure;
+
+public static class LoggingConfig
+{
+    private static readonly string _logPath = Path.Combine(
+        AppContext.BaseDirectory, "logs", "dirtnithm.log");
+
+    public static void Configure()
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.File(_logPath, rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+    }
+}
